a844694aded14778c40bd7af94ef239355c0fe5e
208410190 <208410190@gms.tku.edu.tw>
2020-10-14 22:05:15 +0800

    Updates

---
+++ precache-manifest.996111bf3ae8309bd5c79e5897650d7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d5f8d230e068d05916b2f24d3d4144",
+    "url": "//kentung52.github.io/1091-208410190-robofriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "//kentung52.github.io/1091-208410190-robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b52b5d7c2417af289a0b",
+    "url": "//kentung52.github.io/1091-208410190-robofriends/static/css/main.28accb84.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "//kentung52.github.io/1091-208410190-robofriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//kentung52.github.io/1091-208410190-robofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b52b5d7c2417af289a0b",
+    "url": "//kentung52.github.io/1091-208410190-robofriends/static/js/main.20321691.chunk.js"
+  },
+  {
+    "revision": "2164df3529fadda80a9a",
+    "url": "//kentung52.github.io/1091-208410190-robofriends/static/js/runtime-main.4b3fa047.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "//kentung52.github.io/1091-208410190-robofriends/static/media/SEGA.82e09f78.TTF"
+  }
+]);
